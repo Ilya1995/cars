e6842360912bdbf1208969488becab436480ad48
ilya1995 <kovalyov.king@yandex.ru>
2020-03-03 23:14:52 +0400

    Updates

---
+++ precache-manifest.a4b922b8cd7819f862ca3db3f4652839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50319a50fc5cd1bc6b1db3d6a8497e1",
+    "url": "/cars/index.html"
+  },
+  {
+    "revision": "1850bb5051b8afe2263c",
+    "url": "/cars/static/css/main.8570b4a9.chunk.css"
+  },
+  {
+    "revision": "e2dce02fb60f6c823a2b",
+    "url": "/cars/static/js/2.a8d1a00b.chunk.js"
+  },
+  {
+    "revision": "86f3d4f309858e3852400d83b631a76b",
+    "url": "/cars/static/js/2.a8d1a00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1850bb5051b8afe2263c",
+    "url": "/cars/static/js/main.33beabbb.chunk.js"
+  },
+  {
+    "revision": "629a33d7ea98051aa86f",
+    "url": "/cars/static/js/runtime-main.a5d337b5.js"
+  }
+]);
